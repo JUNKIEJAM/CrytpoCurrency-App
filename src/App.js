f2da0b538a656db1dc15dcbd28969e84b074b7b0
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-19 23:03:51 +0530

    Crytpo Image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Switch,Route,Link} from 'react-router,dom';
 import {Layout,Typography,Space} from antd;
+import {Navbar,Layout,Footer} from './components';
 
 const App = () => {
 
