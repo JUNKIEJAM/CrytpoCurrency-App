0d329c058b7f35abeeabf37072cf23eabed128bd
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-20 18:32:48 +0530

    Overview of Data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import {Typography,Row,Col,Statistic} from 'antd';
 import {Link} from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
+
 const {Title }=Typography;
 
 const Homepage=()=> {
@@ -23,15 +24,15 @@ const Homepage=()=> {
       </Title>
 
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies"  value="5"/></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies"  value={globalStats.total}/></Col>
 
-        <Col span={12}><Statistic title="Total Exchanges"  value={globalStats.total}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges"  value={millify(globalStats.totalExchanges)}/></Col>
 
-        <Col span={12}><Statistic title="Total Market Cap"  value="5"/></Col>
+        <Col span={12}><Statistic title="Total Market Cap"  value={millify(globalStats.totalMarketCap)}/></Col>
 
-        <Col span={12}><Statistic title="Total 24hr Volume"  value="5"/></Col>
+        <Col span={12}><Statistic title="Total 24hr Volume"  value={millify(globalStats.total24hVolume)}/></Col>
 
-        <Col span={12}><Statistic title="Total Markets"  value="5"/></Col>
+        <Col span={12}><Statistic title="Total Markets"  value={millify(globalStats.totalMarkets)}/></Col>
 
 
       </Row>
